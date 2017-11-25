9846289c65ffebf36553f5b3e91110d80c854886
tracykaur <tracykaur@yahoo.com>
2017-11-25 23:33:16 +1000

    konami pop up

---
+++ src/components/Konami.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
+
+class Konami extends Component {
+  constructor() {
+    super ();
+  }
+componentDidMount() {
+  Mousetrap.bind([`1 2 3 4`, `up up down down left right b a`], this.popUp);
+}
+
+componentWillUnMount() {
+  Mousetrap.unbind([`1 2 3 4`, `up up down down left right b a`]);
+
+}
+
+popUp() {
+  alert('The Konami code happened!');
+}
+  render() {
+    return (
+      <div>Hello this is Konami</div>
+    );
+  }
+}
+
+export default Konami;
